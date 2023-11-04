ed240bf31c477ed5858ba36fdde3453a6f71231e
Vincent Ratzke <82654651+lexx257@users.noreply.github.com>
2023-11-04 13:27:26 +0100

    Update InstallEverything.ps1

--- InstallEverything.ps1
+++ InstallEverything.ps1
@@ -1,9 +1,10 @@
 winget source update
 winget upgrade --all --include-unknown 
-winget install Microsoft.Powershell.Preview --force --silent --uninstall-previous
-winget install Alex313031.Thorium --force --silent --uninstall-previous
-winget install Discord.Discord --force --silent --uninstall-previous
-winget install Valve.Steam --force --silent --uninstall-previous
-winget install ndd.Notepad-- --force --silent --uninstall-previous
-winget install Spotify.Spotify --force --silent --uninstall-previous
+winget install Microsoft.Powershell.Preview --silent --uninstall-previous
+winget install Alex313031.Thorium --silent --uninstall-previous
+winget install Discord.Discord --silent --uninstall-previous
+winget install Valve.Steam --silent --uninstall-previous
+winget install ndd.Notepad --silent --uninstall-previous
+winget install Spotify.Spotify--silent --uninstall-previous
+winget install TechPowerUp.NVCleanstall --silent --uninstall-previous
 irm https://christitus.com/win | iex
